03301fb69f926786870ebbae7c8accc329374ff8
九歌 <1169322287@qq.com>
2021-04-13 15:30:27 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 //配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import Users from './components/user/Users.vue'
 
 Vue.use(Router)
 
@@ -9,12 +11,21 @@ const router =  new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component:Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{ path: '/welcome', component: Welcome },
+      { path: '/users', component: Users}]
+    }
   ]
 })
 
 //挂载路由导航守卫
-router.beforeEach((to, form, next) => {
+//to 将要访问的路径
+//from 代表从哪个路径跳转而来
+//next 是一个函数，表示放行
+router.beforeEach((to, from, next) => {
   if (to.path === '/login') return next();
   const tokenStr = window.sessionStorage.getItem('token')
   if (!tokenStr) return next('/login')
